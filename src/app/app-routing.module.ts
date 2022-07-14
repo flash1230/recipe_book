91f53224630756fac13e8c6131f4fe773ee8681d
Mukul <Mukul>
2022-07-14 16:36:55 +0530

    added auth guard for shopping list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth/auth/auth.guard";
 
 const appRoutes: Routes = [ 
     { path:'', redirectTo: '/recipes', pathMatch: 'full'},
     { path:'recipes', 
     loadChildren: () => import('./recipes/recipe.module').then(m=> m.RecipesModule) },
-    { path:'shopping-list', 
+    { path:'shopping-list', canActivate: [AuthGuard],
     loadChildren: () => import('./shoppoing-list/shopping-list.module').then(m=> m.ShoppingListModule) },
     { path:'auth', 
     loadChildren: () => import('./auth/auth/auth.module').then(m=> m.AuthModule) }
