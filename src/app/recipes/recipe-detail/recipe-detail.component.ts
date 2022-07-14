9f14542be3efdedceb42eb9c475849b49847f3df
Mukul <Mukul>
2022-07-14 15:27:42 +0530

    code cleaning

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -9,28 +9,27 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  recipe:Recipe;
+  recipe: Recipe;
   id: number;
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router ) { 
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) {
 
   }
 
   ngOnInit() {
     this.route.params.subscribe(
-      (params: Params)=>{
-        this.id= +params['id'];
+      (params: Params) => {
+        this.id = +params['id'];
         this.recipe = this.recipeService.getRecipe(this.id);
       }
     );
   }
-  OnAddtoShoppinglist()
-  {
+  OnAddtoShoppinglist() {
     this.recipeService.addIngridientstoShoppingList(this.recipe.ingridients)
   }
-  OnEditRecipe(){
-    this.router.navigate(['edit'], {relativeTo: this.route})
+  OnEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route })
   }
-  OnDeleteRecipe(){
+  OnDeleteRecipe() {
     this.recipeService.deleteRecipe(this.id);
     this.router.navigate(['/recipes']);
   }
